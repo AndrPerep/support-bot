8f1913d9dda58199154b5ecd08a5ef91c99f7ec2
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2022-09-10 15:30:59 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,66 +1,4 @@
-# import os
-# import telegram
-#
-# from dotenv import load_dotenv
-#
-#
-# if __name__ == '__main__':
-#     load_dotenv()
-#     tg_token = os.getenv('TG_TOKEN')
-#     tg_chat_id = os.getenv('TG_CHAT_ID')
-#
-#     bot = telegram.Bot(tg_token)
-#
-#     bot.send_message(chat_id=tg_chat_id, text='hi')
-#     print('hi')
-
-# _________________________________________________________________________________________________________________
-# from telegram import ForceReply, Update
-# from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-#
-#
-#
-# # Define a few command handlers. These usually take the two arguments update and
-# # context.
-# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Send a message when the command /start is issued."""
-#     user = update.effective_user
-#     await update.message.reply_html(
-#         rf"Hi {user.mention_html()}!",
-#         reply_markup=ForceReply(selective=True),
-#     )
-#
-#
-# async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Send a message when the command /help is issued."""
-#     await update.message.reply_text("Help!")
-#
-#
-# async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Echo the user message."""
-#     await update.message.reply_text(update.message.text)
-#
-#
-# def main() -> None:
-#     """Start the bot."""
-#     # Create the Application and pass it your bot's token.
-#     application = Application.builder().token("TOKEN").build()
-#
-#     # on different commands - answer in Telegram
-#     application.add_handler(CommandHandler("start", start))
-#     application.add_handler(CommandHandler("help", help_command))
-#
-#     # on non command i.e message - echo the message on Telegram
-#     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-#
-#     # Run the bot until the user presses Ctrl-C
-#     application.run_polling()
-#
-#
-# if __name__ == "__main__":
-#     main()
-
-#_________________________________________________________________
+import json
 
 import telegram
 from telegram.error import NetworkError, Unauthorized
@@ -71,13 +9,33 @@ import os
 
 update_id = None
 
+def get_answer(user_message, language_code='ru-RU'):
+    from google.cloud import dialogflow
 
-def main():
+    session_client = dialogflow.SessionsClient()
+
+    project_id = os.getenv('DIALOG_FLOW_PROJECT_ID')
+    session_id=os.getenv('TG_CHAT_ID')
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=user_message, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
+
+
+def start_bot():
     global update_id
 
     load_dotenv()
     tg_token = os.getenv('TG_TOKEN')
-    tg_chat_id = os.getenv('TG_CHAT_ID')
     bot = telegram.Bot(tg_token)
 
     # get the first pending update_id, this is so we can skip over it in case
@@ -86,7 +44,6 @@ def main():
         update_id = bot.getUpdates()[0].update_id
     except IndexError:
         update_id = None
-
 
     while True:
         try:
@@ -102,14 +59,58 @@ def echo(bot):
     global update_id
     # Request updates after the last update_id
     for update in bot.getUpdates(offset=update_id, timeout=10):
-        # chat_id is required to reply to any message
-        chat_id = update.message.chat_id
         update_id = update.update_id + 1
 
         if update.message:  # your bot can receive updates without messages
             # Reply to the message
-            update.message.reply_text(update.message.text)
+            print(update.message.text)
+            update.message.reply_text(get_answer(update.message.text))
+
+
+def create_intent(display_name, intent_phrases):
+    from google.cloud import dialogflow
+
+    load_dotenv()
+
+    training_phrases_parts = intent_phrases['questions']
+    message_texts = [intent_phrases['answer']]
+    project_id = os.getenv('DIALOG_FLOW_PROJECT_ID')
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def create_intents():
+    phrases_filename = 'phrases.json'
+    with open(phrases_filename, 'r', encoding='utf-8') as phrases_file:
+        phrases_json = phrases_file.read()
+
+    phrases = json.loads(phrases_json)
+
+    for display_name, intent_phrases in phrases.items():
+        create_intent(display_name, intent_phrases)
 
 
 if __name__ == '__main__':
-    main()
+    start_bot()
+    # create_intents()
